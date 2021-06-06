442aff07eacf0c095a8a9a2895b47bc09402fcb0
Hanzala Arfat <hanzala735@gmail.com>
2021-06-06 09:28:45 +0530

    first commit

---
+++ src/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "../Login/Login";
+import "./Home.css";
+function Home() {
+  return (
+    <div className="home">
+      <div className="login">
+        <Login />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
